4d65ba85b4686de5cd2a74d228cccd662977c5b6
KoNstantin_Binevskii <kbynevskij@yandex.ru>
2024-10-18 00:13:53 +0200

    кнопка

---
+++ main.js
@@ -0,0 +1,42 @@
+const data = JSON.parse(dataInfo);
+const cartBox = document.querySelector('.cart-box');
+
+data.forEach(element => {
+    const bodyEl = document.createElement('div');
+    bodyEl.classList.add('bodyContent'); // Добавляем класс container
+
+    const itemImg = document.createElement('img');
+    itemImg.src = element.url;
+    itemImg.alt = 'Add to Cart';
+
+    const itemContent = document.createElement('div');
+    itemContent.classList.add('item__content');
+
+    const itemHead = document.createElement('h4');
+    itemHead.classList.add('head');
+    itemHead.textContent = element.name;
+
+    const itemText = document.createElement('p');
+    itemText.classList.add('other');
+    itemText.textContent = element.textInfo;
+
+    const itemPrice = document.createElement('p');
+    itemPrice.classList.add('price');
+    itemPrice.textContent = `$${element.price}`;
+
+    const button = document.createElement('button');
+    button.classList.add('button');
+    const icon = document.createElement('i');
+    icon.classList.add('fas', 'fa-shopping-cart');
+    button.appendChild(icon);
+    const buttonText = document.createTextNode(' Add to Cart');
+    button.appendChild(buttonText);
+
+    cartBox.appendChild(bodyEl);
+    bodyEl.appendChild(itemImg);
+    bodyEl.appendChild(itemContent);
+    bodyEl.appendChild(button);
+    itemContent.appendChild(itemHead);
+    itemContent.appendChild(itemText);
+    itemContent.appendChild(itemPrice);
+});
